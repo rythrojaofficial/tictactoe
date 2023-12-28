c5ec8acf3bc7c2ca67fcc1f326b2c33d8e9b628f
rythroja <jared@mercenarytricking.com>
2023-12-27 16:30:50 -0800

    list total spots on the game board and add a method to remove a spot if it is taken

--- script.js
+++ script.js
@@ -9,9 +9,15 @@ function createPlayer(name){
     let userName = name;
     let score = 0;
     let marker = 'x';
+    let plays = [];
 
     function getScore(){return score}; 
     function improveScore(){return score++};
+    function getPlays(){return plays};
+    function addPlays(spot){
+        this.plays.push(spot);
+        return plays;
+    }
     function changeMarker(){
         switch(this.marker){
             case 'x': this.marker = 'o'; break;
@@ -19,7 +25,7 @@ function createPlayer(name){
             default: console.log(throwError);
         }
     }
-    return {userName, score, marker, getScore, improveScore, changeMarker}
+    return {userName, score, marker, plays, getScore, improveScore, getPlays, addPlays, changeMarker}
     
 }
 
@@ -34,25 +40,80 @@ const theComputer = createPlayer('theComputer');
 // =========
 // the game 
 // =========
+
 const theGame = (function(){
     let currentGame = {};
 
-})
+    
+    function playerRound(){
+        let playerChoice = parseInt(prompt('choose a spot on your numpad'));
+        return player1.addPlays(playerChoice);
+    }
+
+    function computerRound(){
+        let computerChoice = Math.random
+    }
+    return{
+        currentGame, playerRound
+    }
+})()
+
+const theBoard = (function(){
+    let availableSpots = [1,2,3,4,5,6,7,8,9];
+    let spots = availableSpots.length;
+
+    
+    function getSpots(){return spots};
+    function available(){return availableSpots};
+    function removeSpots(selection){
+        const index = availableSpots.indexOf(selection);
+        const x = availableSpots.splice (index,1);
+        return availableSpots;
+    };
+
+    return {getSpots, available, removeSpots}
+})()
+
+const playerRound = (function(){
+    let playerChoice = parseInt(prompt('choose a spot on your numpad'));
+    if (!player1.getPlays().includes(playerChoice)
+        && !theComputer.getPlays().includes(playerChoice)){
+        return player1.addPlays(playerChoice);
+    }else playerChoice = parseInt(prompt('choose a spot not taken'));
+     
+})();
+
+const computerRound = (function(){
+    // produces number between 1 and 9
+    let computerChoice = Math.floor((Math.random() * 9) + 1);
+    if(!player1.getPlays().includes(computerChoice)
+    && !theComputer.getPlays().includes(computerChoice)){
+        return theComputer.addPlays(computerChoice);
+    }
+    
+    
+})();
+theBoard.removeSpots(5);
 
 
 
+// player1.addPlays(2);
+// player1.addPlays(5);
+// player1.addPlays(8);
 console.log({
     player: player1.userName,
     marker: player1.marker,
-    score: player1.getScore()
+    score: player1.getScore(),
+    plays: player1.getPlays()
 });
 
 console.log({
     player: theComputer.userName,
     marker: theComputer.marker,
-    score: theComputer.getScore()
+    score: theComputer.getScore(),
+    plays: theComputer.getPlays()
 });
-
+console.log(theBoard.available());
 
     
 // ================== 
